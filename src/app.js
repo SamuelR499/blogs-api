35bfcedcc52da350522be16448c3dccd96a4b569
Samuelr499 <samuelribeiro2@hotmail.com>
2022-09-26 14:15:20 -0300

    aplica middleware de erro

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const errorMiddleware = require('./middlewares/errorMidleware');
 const authRouter = require('./routes/authRouter');
 // ...
 
@@ -7,6 +8,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/login', authRouter);
+
+app.use(errorMiddleware);
+
 // ...
 
 // É importante exportar a constante `app`,

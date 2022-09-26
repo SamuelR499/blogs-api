28249c23f5c32f396deb738feeaaa443e33c03a0
Samuelr499 <samuelribeiro2@hotmail.com>
2022-09-26 20:34:24 -0300

    feat: creatUser

---
+++ src/services/userService.js
@@ -0,0 +1,60 @@
+const { User } = require('../models');
+const errorGenerate = require('../utils/genericErrorHanlder');
+const { generateToken } = require('../utils/JWT');
+const { loginSchema, createUserSchema } = require('../utils/schemas');
+
+const userIsValid = (payload) => {
+    const { error } = loginSchema.validate(payload);
+
+    if (error) {
+        throw errorGenerate(400, error.message);
+    }
+};
+
+const userExist = async (email) => {
+    const result = await User.findOne({
+        attributes: ['id', 'displayName', 'email', 'image'],
+        where: { email },
+    });
+    return result;
+};
+
+const authenticate = async ({ email, password }) => {
+    userIsValid({ email, password });
+
+    const user = await User.findOne({
+        attributes: ['id', 'displayName', 'email', 'image'],
+        where: { email, password },
+    });
+
+    if (!user) {
+        const error = errorGenerate(400, 'Invalid fields');
+
+        throw error;
+    }
+
+    const token = generateToken(user.dataValues);
+
+    return { token };
+};
+
+    const creatUser = async (user) => {
+        const { error } = createUserSchema.validate(user);
+
+        if (error) throw errorGenerate(400, error.message);
+
+        const isUser = await userExist(user.email);
+
+        if (isUser !== null) throw errorGenerate(409, 'User already registered');
+
+        const newUser = await User.create({ ...user });
+        console.log('newUser >_ ', newUser.dataValues);
+        const token = generateToken(newUser.dataValues);
+
+        return { token };
+    };
+
+module.exports = {
+    authenticate,
+    creatUser,
+};

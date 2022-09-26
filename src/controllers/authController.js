f4ffc5169000f07a07adb0764fbd4b2e793dabe9
Samuelr499 <samuelribeiro2@hotmail.com>
2022-09-26 14:13:36 -0300

    acrescenta try catch para  erros, para a aplicação não quebrar

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,12 @@
 const authService = require('../services/authService');
 
-const auth = async (req, res) => {
+const auth = async (req, res, next) => {
+try {
     const authToken = await authService.authenticate(req.body);
     return res.status(200).json(authToken);
+} catch (error) {
+    next(error);
+}
 };
 
 module.exports = {

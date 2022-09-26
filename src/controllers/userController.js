71498535a0bd33cb362f6e9096cbf9e7765e227f
Samuelr499 <samuelribeiro2@hotmail.com>
2022-09-26 20:33:12 -0300

    feat: userController

---
+++ src/controllers/userController.js
@@ -0,0 +1,23 @@
+const userService = require('../services/userService');
+
+const auth = async (req, res, next) => {
+    try {
+        const authToken = await userService.authenticate(req.body);
+        return res.status(200).json(authToken);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const createUser = async (req, res, next) => {
+    try {
+        const token = await userService.creatUser(req.body);
+        return res.status(201).json(token);
+    } catch (error) {
+        next(error);
+    }
+};
+module.exports = {
+    auth,
+    createUser,
+};

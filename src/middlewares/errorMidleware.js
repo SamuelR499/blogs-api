343959fdd85379417f311daa9d7fec326a66490b
Samuelr499 <samuelribeiro2@hotmail.com>
2022-09-27 16:10:06 -0300

    refactor: adiciona return no if, para o codigo não continuar

--- src/middlewares/errorMidleware.js
+++ src/middlewares/errorMidleware.js
@@ -1,6 +1,6 @@
 const errorMiddleware = (err, _req, res, _next) => {
     const { status, message } = err;
-    if (status) res.status(status).json({ message });
+    if (status) return res.status(status).json({ message });
 
     return res.status(500).json({ message: 'Internal Server Error', error: message });
 };

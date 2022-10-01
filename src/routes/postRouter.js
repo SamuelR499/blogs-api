10a693efe4e4fd326a942274a68eb27fd7f859c7
Samuelr499 <samuelribeiro2@hotmail.com>
2022-10-01 13:29:37 -0300

    feat: addPost route

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -5,6 +5,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.get('/:id', authMiddleware, postController.findById);
+// router.put('/:id', authMiddleware, postController.updatePost);
 
 router.get('/', authMiddleware, postController.getPosts);
 router.post('/', authMiddleware, postController.addPost);
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,6 +1,14 @@
-const { BlogPost, User, Category } = require('../models');
+const { BlogPost, User, Category, PostCategory, sequelize } = require('../models');
 const errorGenerate = require('../utils/genericErrorHanlder');
 const { createPostSchema } = require('../utils/schemas');
+
+const categoryExists = async (categoryIds) => {
+  const category = await (await Promise.all(categoryIds.map((id) => (
+    Category.findOne({ where: { id } })
+  )))).every((elem) => elem !== null);
+
+  return category;
+};
 
 const getPosts = async () => {
   const posts = await BlogPost.findAll({
@@ -21,13 +29,30 @@ const getPosts = async () => {
   return posts;
 };
 
-const addPost = async (post) => { 
+const addPost = async (post, userId) => { 
+  const { title, content, categoryIds } = post;
   const { error } = createPostSchema.validate(post);
 
   if (error) throw errorGenerate(400, 'Some required fields are missing');
 
-  const newPost = await post;
-  return newPost;
+  const isCategory = await categoryExists(categoryIds);
+  if (!isCategory) throw errorGenerate(400, '"categoryIds" not found');
+
+  const result = await sequelize.transaction(async (t) => {
+    const newPost = await BlogPost.create(
+        { title, content, userId, categoryIds },
+        { transaction: t },
+    );
+
+    console.log('newPost ---------->', newPost.dataValues);
+    const postId = newPost.dataValues.id;
+
+    await Promise.all(categoryIds.map(async (id) => PostCategory
+    .create({ postId, categoryId: id }, { transaction: t })));
+    return newPost;
+  });
+  console.log('result ---------->', result);
+return result;
 };
 
 const findById = async (id) => {
